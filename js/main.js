8b82bbf0429e588550cfcfeb571c50dadb9241ee
tpielaszkiewicz <tpielaszkiewicz@gmail.com>
2017-09-01 00:02:21 +0200

    https

--- js/main.js
+++ js/main.js
@@ -13,13 +13,13 @@ function assign_handler() {
         var firma = $('#firma');
 
         // uzywajac samego get JSon
-        $.getJSON('http://echo.jsontest.com/imie/Piotr/nazwisko/Lewinski/zawod/Programista/firma/Akademia108', function (resultJSON) {
+        $.getJSON('https://echo.jsontest.com/imie/Piotr/nazwisko/Lewinski/zawod/Programista/firma/Akademia108', function (resultJSON) {
             // wyswietl w konsoli
             console.log(resultJSON);
         });
         // funkcja jQuery
         $.ajax({
-            url: "http://echo.jsontest.com/imie/Piotr/nazwisko/Lewinski/zawod/Programista/firma/Akademia108",
+            url: "https://echo.jsontest.com/imie/Piotr/nazwisko/Lewinski/zawod/Programista/firma/Akademia108",
             dataType: 'json',
             success: function (resultJSON) {
                 imie.text("Imię to: " + resultJSON.imie);
